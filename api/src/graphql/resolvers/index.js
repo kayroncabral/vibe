dc16e438e99286dc2290caab8c8e10945f5e8a17
Kayron Cabral <kayron.scabral@gmail.com>
2020-09-27 14:56:25 -0400

    ✨ Filter by statys and date

--- api/src/graphql/resolvers/index.js
+++ api/src/graphql/resolvers/index.js
@@ -1,13 +1,15 @@
 import Mutation from 'src/graphql/resolvers/Mutation'
 import Node from 'src/graphql/resolvers/Node'
 import Query from 'src/graphql/resolvers/Query'
+import Schedule from 'src/graphql/resolvers/Schedule'
 import User from 'src/graphql/resolvers/User'
 
 const resolvers = {
   Query,
   Mutation,
   Node,
-  User
+  User,
+  Schedule
 }
 
 export default resolvers
--- api/src/graphql/schedule/queries.js
+++ api/src/graphql/schedule/queries.js
@@ -1,16 +1,35 @@
+import { startOfDay, endOfDay } from 'date-fns'
+
 import { Schedule } from 'src/models'
 
-import { ScheduleStatus } from 'src/utils/enums'
-
 export const schedule = async () => {
-  return {
-    doctor: '123123',
-    patient: '123123',
-    date: new Date('09/28/2020 13:00'),
-    status: ScheduleStatus.SCHEDULED
-  }
+  return Schedule.findOne({})
 }
 
-export const schedules = async () => {
-  return Schedule.find({})
+export const schedules = async (parent, { input }, context, info) => {
+  const doctor = '5f70c3a4d86a530a98f904ce'
+
+  const conditions = { doctor /* $text: { $search: 'Kayron' } */ }
+
+  if (input.filter?.status) conditions.status = { $in: input.filter.status }
+
+  if (input.filter?.start || input.filter?.end) {
+    conditions.date = {}
+
+    if (input.filter?.start) {
+      const start = new Date(input.filter.start)
+      conditions.date.$gte = startOfDay(start).toISOString()
+
+      if (!input.filter?.end) conditions.date.$lte = endOfDay(start).toISOString()
+    }
+
+    if (input.filter?.end) {
+      const end = new Date(input.filter.end)
+      conditions.date.$lte = endOfDay(end).toISOString()
+
+      if (!input.filter?.start) conditions.date.$gte = startOfDay(end).toISOString()
+    }
+  }
+
+  return Schedule.find(conditions).populate('patient')
 }
