2e3eda820a0905bf81b1ef4f375d01c120e2b8a1
Kayron Cabral <kayron.scabral@gmail.com>
2020-09-30 07:30:40 -0400

    🐛 Fix seed

--- api/seed.sh
+++ api/seed.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mongoimport mongodb://localhost:27017/vibe --collection schedules --type json --file seed/users.json
-mongoimport mongodb://localhost:27017/vibe --collection schedules --type json --file seed/schedules.json
+mongoimport --db vibe --collection users --drop --type json --file seed/users.json --jsonArray
+mongoimport --db vibe --collection schedules --drop  --type json --file seed/schedules.json --jsonArray
